c52df32cac89b246d7634471a6aaf4e85f8014d6
Swathisp43 <>
2024-08-03 21:43:22 +0530

    Second Commit

--- src/test/java/testCases/TC_001_ValReccReimbursment.java
+++ src/test/java/testCases/TC_001_ValReccReimbursment.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -33,42 +34,23 @@ public class TC_001_ValReccReimbursment {
 		js.executeScript("arguments[0].scrollIntoView();", calculator);
 
 		//4) Adjust the Slider: Get the slider dimensions and position
-		int sliderWidth = slider.getSize().getWidth();
-		WebElement sliderHandle = driver.findElement(By.xpath("//span[contains(@class, 'MuiSlider-thumb')]"));
-
-		// Check if aria-valuenow attribute is present
-		String ariaValueNow = sliderHandle.getAttribute("aria-valuenow");
-		int currentValue = 200; // Default value if aria-valuenow is not available
-		if (ariaValueNow != null) {
-		    try {
-		        currentValue = Integer.parseInt(ariaValueNow);
-		    } catch (NumberFormatException e) {
-		        System.out.println("Failed to parse aria-valuenow: " + ariaValueNow);
-		    }
-		}
-
-		// Calculate the x-offset to set the slider to 820
-		int maxSliderValue = 1000; // Assume the maximum value for the slider is 1000
-		int currentOffset = (currentValue * sliderWidth) / maxSliderValue;
-		int desiredOffset = (820 * sliderWidth) / maxSliderValue;
-		int xOffset = desiredOffset - currentOffset;
-
-		// Move the slider to 820
-		Actions move = new Actions(driver);
-		move.clickAndHold(sliderHandle).moveByOffset(xOffset, 0).release().perform();
-
-		Thread.sleep(1000); 
+		
+		  WebElement sliderHandle =
+		  driver.findElement(By.xpath("//span[contains(@class, 'MuiSlider-thumb')]"));		  
+		  Actions action = new Actions(driver);
+		  action.dragAndDropBy(sliderHandle,-94,0).perform();		  
+	//	  action.dragAndDropBy(sliderHandle,124,0).perform();
+		Thread.sleep(5000); 
 		
 		// 5) Update the Text Field:		  		  
-		 WebElement slider_input = driver.findElement(By.xpath(
-				"//input[@class='MuiInputBase-input MuiOutlinedInput-input MuiInputBase-inputSizeSmall css-1o6z5ng']"));
-		slider_input.click();
-		slider_input.clear();
+		WebElement slider_input = driver.findElement(By.xpath("//input[@type='number']"));
+		//action.dragAndDropBy(sliderHandle,-94,0).perform();
+		slider_input.sendKeys(Keys.BACK_SPACE);
 		slider_input.sendKeys("560");
 		
-		
+		Thread.sleep(1000);
 		//6) Validate Slider Value:
-		  String sliderValue = slider.getAttribute("value");
+		  String sliderValue = slider_input.getAttribute("value");
           if ("560".equals(sliderValue)) {
               System.out.println("Slider value is correctly set to 560.");
           } else {
@@ -76,29 +58,36 @@ public class TC_001_ValReccReimbursment {
           }
           
           //7)Select CPT Codes:
-
+ 		action.dragAndDropBy(sliderHandle,-94,0).perform();	
+ 		slider_input.sendKeys(Keys.BACK_SPACE);
+ 		slider_input.sendKeys("820");
+ 		
           ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", slider_input);
-          WebElement cpt99091 = driver.findElement(By.xpath("(//input[@type='checkbox'])[1]")); // Replace with the actual ID of the checkbox
-          WebElement cpt99453 = driver.findElement(By.xpath("(//input[@type='checkbox'])[2]")); // Replace with the actual ID of the checkbox
-          WebElement cpt99454 = driver.findElement(By.xpath("(//input[@type='checkbox'])[3]")); // Replace with the actual ID of the checkbox
+          WebElement cpt99091 = driver.findElement(By.xpath("(//input[@type='checkbox'])[1]")); 
+          WebElement cpt99453 = driver.findElement(By.xpath("(//input[@type='checkbox'])[2]")); 
+          WebElement cpt99454 = driver.findElement(By.xpath("(//input[@type='checkbox'])[3]")); 
+          WebElement cpt99474 = driver.findElement(By.xpath("(//input[@type='checkbox'])[8]"));
           if (!cpt99091.isSelected()) cpt99091.click();
           if (!cpt99453.isSelected()) cpt99453.click();
           if (!cpt99454.isSelected()) cpt99454.click();
+          if(!cpt99474.isSelected()) cpt99474.click();
           
-       //8   Validate Total Recurring Reimbursement:
-          WebElement totalReimbursementHeader = driver.findElement(By.xpath("//p[@class='MuiTypography-root MuiTypography-body2 inter css-1xroguk'][contains(text(),'Total Recurring Reimbursement for all Patients Per')]")); // Replace with the actual ID of the header
-          String expectedReimbursement = "Total Recurring Reimbursement for all Patients Per Month: $110700";
+          Thread.sleep(2000);
+       //8 )  Validate Total Recurring Reimbursement:
+          WebElement totalReimbursementHeader = driver.findElement(By.xpath("//p[@class='MuiTypography-root MuiTypography-body1 inter css-hocx5c'][normalize-space()='$110700']"));
+          String expectedReimbursement = "$110700";
           String actualReimbursement = totalReimbursementHeader.getText();
+          System.out.println("actualReimbursement:"+ actualReimbursement);
 
           if (expectedReimbursement.equals(actualReimbursement)) {
               System.out.println("Total Recurring Reimbursement is correctly displayed as $110700.");
           } else {
               System.out.println("Total Recurring Reimbursement is not displayed correctly.");
           }
-		}
-       finally {
           
-      }
+		}
+       finally {        
+        }
       driver.quit();
           
 	}
